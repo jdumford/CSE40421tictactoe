0b23fd9d3e8b79bafd2155ab8339915f937fee59
NicHaydel95 <nhaydel@nd.edu>
2015-09-20 17:58:59 -0400

    Simple tic tac toe game, NO AI

---
+++ TicTacToe.py
@@ -0,0 +1,95 @@
+import sys
+class Game_Board:
+	"""Tic Tac Toe board"""
+	def __init__(self):
+		self.board=[".",".",".",".",".",".",".",".","."]
+		self.p1=[]
+		self.p2=[]
+	def display_board(self):
+		for i in range(len(self.board)):
+			print self.board[i],
+			if ((i+1)%3)==0:
+				print
+	def mark_board(self, place, player):
+		if place>9 or place<1:
+			return False
+		else:
+			if player==1 :
+				if self.empty_cell(place-1):
+					self.board[place-1]="X"
+					self.p1.append(place)
+				else:
+					return False
+			else:
+				if self.empty_cell(place-1):
+					self.board[place-1]="O"
+					self.p2.append(place)
+				else:
+					return False
+			return True
+	def is_empty(self):
+		for e in self.board:
+			if e != ".":
+				return False
+		return True
+	def is_full(self):
+		for e in self.board:
+			if e == ".":
+				return False
+		return True
+	def empty_cell(self,i):
+		if self.board[i]==".":
+			return True
+		else:
+			return False
+	def check_win(self):
+		for i in range(3):
+			#Check vertical
+			if self.board[i]==self.board[i+3]==self.board[i+6] and self.board[i]!=".":
+				return True
+			#Check horizontal
+			if self.board[i*3]==self.board[(i*3)+1]==self.board[(i*3)+2] and self.board[i*3]!=".":
+				return True
+			#Check Diagonal
+			if(i==0):
+				if self.board[i]==self.board[i+4]==self.board[i+8] and self.board[i]!=".":
+					return True
+			#Check Diagonal
+			if(i==2):
+				if self.board[i]==self.board[i+2]==self.board[i+4] and self.board[i]!=".":
+					return True
+		return False
+		
+
+
+x = Game_Board()
+x.display_board
+while True:
+	if x.is_empty():
+		player = 1
+	place = int(input("What spot would you like to mark: "))
+	if x.mark_board(place, player)==False:
+		print "Invalid move, Choose again"
+		continue
+	else:
+		print "Player %d has marked %d: " % (player, place)
+	if x.check_win():
+		if player==1:
+			x.display_board()
+			print "Player 1 wins!"
+			sys.exit()
+		else:
+			x.display_board()
+			print "Player 2 wins!"
+			sys.exit()
+	if player==1:
+		player=2
+	else:
+		player=1
+	x.display_board()
+	if x.is_full():
+		print "The game is a draw"
+		sys.exit()
+
+
+		
