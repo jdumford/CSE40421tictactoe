723424883a575e9fabdba006af9d69b9eb0677b8
Nic <nhaydel@nd.edu>
2015-09-29 10:05:54 -0400

    It plays, but does not win, check the minimax algorithm

--- Minimax.py
+++ Minimax.py
@@ -1,36 +1,42 @@
 def win(board, symbol):
 	for i in range(3):
 		#Check vertical
-		if self.board[i]==self.board[i+3]==self.board[i+6] and self.board[i]==symbol:
+		if board[i]==board[i+3]==board[i+6] and board[i]==symbol:
 			return True
 		#Check horizontal
-		if self.board[i*3]==self.board[(i*3)+1]==self.board[(i*3)+2] and self.board[i*3]==symbol:
+		if board[i*3]==board[(i*3)+1]==board[(i*3)+2] and board[i*3]==symbol:
 			return True
 		#Check Diagonal
 		if(i==0):
-			if self.board[i]==self.board[i+4]==self.board[i+8] and self.board[i]==symbol:
+			if board[i]==board[i+4]==board[i+8] and board[i]==symbol:
 					return True
 			#Check Diagonal
 		if(i==2):
-			if self.board[i]==self.board[i+2]==self.board[i+4] and self.board[i]==symbol:
+			if board[i]==board[i+2]==board[i+4] and board[i]==symbol:
 					return True
 	return False
 def available_moves(game_board):
-	moves=([])
-	for i in range(len(game_board.board)):
-		if game_board.board[i]==".":
-			moves.append(i)
+	moves=[]
+	for i in range(len(game_board)):
+		if game_board[i]==".":
+			moves.append(i+1)
 	return moves
 def get_new_state(board, move, symbol):
-	new_state=board
-	new_state[move]=symbol
+	new_state=[]
+	for i in range(len(board)):
+		new_state.append(board[i])
+	new_state[move-1]=symbol
 	return new_state
 		
-def score(board, symbol, opponent):
+def score(board, symbol):
     if win(board, symbol):
         return 10
-    elif win(board, opponent):
-        return -10
+    if symbol=='X':
+    	if win(board, 'O'):
+        	return -10
+    elif symbol=='O':
+	if win(board,'X'):
+		return -10
     else:
         return 0
     
@@ -40,24 +46,24 @@ def is_full(board):
 				return False
 		return True
 def minimax(board,player,symbol):
-	action=-1
+	moves = available_moves(board)
+	action=1
         if win(board,symbol) or is_full(board):
 		return [score(board,symbol),action]
-        moves = available_moves(board)
         if player == 1:
         	v=-100000
                 for move in moves:
-                        newboard=generate_new_state(board,move,"X")
-                        new_score=minimax(newboard, 2)
-                        if (new_score>v):
+                        newboard=get_new_state(board,move,'X')
+                        new_score=minimax(newboard, 2,'O')[0]
+                        if new_score>v:
                         	action=move
                         	v=new_score
         else:
         	v=1000000
                 for move in moves:
-                        newboard=generate_new_state(board,move,"O")
-                        new_score=minimax(newboard, 1)
-                        if (new_score<v):
+                        newboard=get_new_state(board,move,'O')
+                        new_score=minimax(newboard, 1,'X')[0]
+                        if new_score<v:
                         	action=move
                         	v=new_score
         return [v,action]
--- TicTacToe.py
+++ TicTacToe.py
@@ -15,7 +15,7 @@ class Game_Board:
 		if place>9 or place<1:
 			return False
 		else:
-			if player==1 :
+			if player==2 :
 				if self.empty_cell(place-1):
 					self.board[place-1]="X"
 					self.p1.append(place)
@@ -72,7 +72,7 @@ if num_players==1:
 		if player==1:
 			place = int(input("What spot would you like to mark: "))
 		elif player==2:
-			place=minimax(x.board, 1, 'X')[1] 
+			place=Minimax.minimax(x.board, 2, 'X')[1] 
 		if x.mark_board(place, player)==False:
 			print "Invalid move, Choose again"
 		
@@ -101,9 +101,9 @@ else:
 		if x.is_empty():
 			player = 1
 		if player==1:
-			place = minimax(x.board, 2,'O')[1]
+			place = Minimax.minimax(x.board, 2,'O')[1]
 		elif player==2:
-			place=minimax(x.board, 1, 'X')[1] 
+			place=Minimax.minimax(x.board, 1, 'X')[1] 
 		if x.mark_board(place, player)==False:
 			print "Invalid move, Choose again"
 		
